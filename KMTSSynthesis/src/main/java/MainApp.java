eac2e08521010af856c94965d036af66d4cd220b
Marcos Vinícius Andrade Moreira <marcos12144@hotmail.com>
2016-05-23 15:48:57 -0300

    Adicionando Comentários
    Ainda não terminei.
    Adicionei uma função teste no main para visualizar o ToString de State.
    Fiz uma cobertura temporária de casos de erro com println em State.

--- KMTSSynthesis/src/main/java/MainApp.java
+++ KMTSSynthesis/src/main/java/MainApp.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import kmts.KMTS;
 import kmts.element.State;
+import kmts.element.Transition;
 import logic.ThreeLogicResolver;
 import logic.booleanexpression.AndBooleanExpression;
 import logic.booleanexpression.AtomicProposition;
@@ -25,8 +26,9 @@ public class MainApp {
 		//testKMTS();
 		//testSequenceDiagram();
 		//testBooleanExpression();
-		testValuationToExpression();
+		//testValuationToExpression();
 		//testPermutation();
+                testStateToString();
 	}
 	
 	private static void testValuationToExpression() 
@@ -178,4 +180,21 @@ public class MainApp {
 		generatedKMTSs.toString();
 	}
 	
+        //Testa o metodo 'ToString' da class 'State'
+        private static void testStateToString()
+        {
+                State s = new State();
+                s.setLabel("Q1");
+                Transition t = new Transition();
+                t.setAction("A");
+                s.addInTrasition(t);
+                Transition t1 = new Transition();
+                t1.setAction("B");
+                s.addOutTrasition(t1);
+                s.addPreposition("X", Boolean.TRUE);
+                s.addPreposition("Y", Boolean.FALSE);
+                s.addPreposition("Z", Boolean.TRUE);
+                s.addPreposition("W", Boolean.FALSE);
+                System.out.println(s.toString());
+        }
 }
--- KMTSSynthesis/src/main/java/kmts/KMTS.java
+++ KMTSSynthesis/src/main/java/kmts/KMTS.java
@@ -9,31 +9,47 @@ import kmts.element.Transition;
 
 public class KMTS {
 
+        //O KMTS tem um rotulo
+        //é constituido de:
+        //estado inicial e conjunto de estados
+        //conjunto de transições entre os estados
+        //um boolean pra dizer se o KMTS é deterministico
+        //um boolean pra diser se e é permitido duplicar rotulos de estados
 	private String label;
 	private State initialState;
 	private Set<State> states;
 	private Set<Transition> transitions;
 	private boolean deterministic;
 	private boolean allowDuplicatedStateLabels;
-
 	
+        //Constroi um KMTS recebendo o rotulo
 	public KMTS(String label) {
 		super();
+                this.label = label;
 		states = new HashSet<State>();
 		transitions = new HashSet<Transition>();
 		deterministic = true;
 		allowDuplicatedStateLabels = false;
 	}
+        
+        //Retorna o estado inicial do KMTS
 	public State getInitialState() {
 		return initialState;
 	}
+        
+        //Retorna todos os estados do KMTS
 	public Set<State> getStates() {
 		return states;
 	}
+        
+        //Retorna todas as trasições do KMTS
 	public Set<Transition> getTransitions() {
 		return transitions;
 	}
 	
+        //Recebe o estado de partida e o de chegada da nova transição e o rotulo dela.
+        //Cria uma trasição 'may' e tranforma em must.
+        //Retorna a transição criada. ??para teste??
 	public Transition addMustTransitionBetween(State fromState, State toState, String action)
 	{
 		Transition t = addMayTransitionBetween(fromState, toState, action);
@@ -41,6 +57,11 @@ public class KMTS {
 		return t;
 	}
 	
+        //Recebe o estado de partida e o de chegada da nova transição e o rotulo dela.
+        //Checa se uma trasição 'may' pode ser addicionada entre os estados dados.
+        //Cria uma transição.
+        //adiciona a trasição na saida e chegada dods estados recebidos.
+        //Retorna a transição criada. ??para teste??
 	public Transition addMayTransitionBetween(State fromState, State toState, String action)
 	{
 		checkCanAddMayTransitionBetween(fromState, toState, action);
@@ -55,12 +76,14 @@ public class KMTS {
 		return t;
 	}
 	
-	
+	//Decidir o que faz e como faz.
 	private void checkCanAddMayTransitionBetween(State fromState,
 			State toState, String action) {
 		// TODO Auto-generated method stub
 		
 	}
+        
+        //Adiciona ao KMTS o estado que recebe como parametro.
 	public void addState(State state)
 	{
 		checkCanAddState(state);
@@ -68,15 +91,20 @@ public class KMTS {
 		states.add(state);
 	}
 	
+        //Adiciona um estado inicial
 	public void addInitialState(State state)
 	{
 		addState(state);
 		initialState = state;
 	}
 	
+        //Decidir o que faz e como faz.
 	private void checkCanAddState(State state) {
 		// TODO Auto-generated method stub
 	}
+        
+        //Busca estado pelo rotulo.
+        //Como 'iterator' funciona?
 	public State getStateByLabel(String label)
 	{
 		if (states != null)
@@ -94,6 +122,19 @@ public class KMTS {
 		return null;
 	}
 	
+        //Imprime as informações do KMTS no padrao:
+        //LABEL: <rotulo do KMTS>
+        //INITIAL STATE: <rotulo do estado inicial>
+        //STATES: <rotulo de todos estados>
+        //TRANSITIONS: <todas transições>
+        //<estadoDe.rotulo transição.tipo estatoPara.rotulo>
+        //EX:
+            //LABEL: null
+            //INITIAL STATE: S0
+            //STATES: S1, S0
+            //TRANSITIONS:
+            //S1 --> S0
+            //S0 ==> S1
 	@Override
 	public String toString()
 	{
@@ -136,9 +177,13 @@ public class KMTS {
 		result.append("------------\n");
 		return result.toString();
 	}
+        
+        //Retorna o rotulo do KMTS
 	public String getLabel() {
 		return label;
 	}
+        
+        //Troca o rotudo do KMTS pelo recebido.
 	public void setLabel(String label) {
 		this.label = label;
 	}
